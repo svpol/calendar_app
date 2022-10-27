9078abe039826642ed5417a85c64145be05f8247
Svetlana Polyanskaya <s.s.polyanskaya@gmail.com>
2022-10-27 16:04:28 +0300

    Absolute minute system is introduced

--- calendar.py
+++ calendar.py
@@ -1,12 +1,26 @@
 from datetime import time
 
 
+DAYTIME = list(range(1440))
+
+
 class Calendar:
 
     @staticmethod
-    def get_working_time(working_time):
-        day_start = time(working_time[0][0], working_time[0][1])
-        day_end = time(working_time[1][0], working_time[1][1])
+    def _map_time_to_minutes(time_list):
+        abs_miniute = time_list[0] * 60 + time_list[1]
+        return abs_miniute
+
+    @staticmethod
+    def working_time_minutes(working_time_list):
+        day_start = Calendar._map_time_to_minutes(working_time_list[0])
+        day_end = Calendar._map_time_to_minutes(working_time_list[1])
+        return DAYTIME[day_start:day_end+1]
+
+    @staticmethod
+    def get_working_time(working_time_list):
+        day_start = time(working_time_list[0][0], working_time_list[0][1])
+        day_end = time(working_time_list[1][0], working_time_list[1][1])
         return [day_start, day_end]
 
     @staticmethod
@@ -15,14 +29,15 @@ class Calendar:
 
     @staticmethod
     def book_timeslot(calendar, time_start, time_end):
-        updated_calendar = []
-        time_start_time = time(time_start[0], time_start[1])
-        time_end_time = time(time_end[0], time_end[1])
-        if time_start_time > calendar[0]:
-            slot_before = [calendar[0], time_start_time]
-            slot_after = [time_end_time, calendar[1]]
+        time_start_minutes = Calendar._map_time_to_minutes(time_start)
+        time_end_minutes = Calendar._map_time_to_minutes(time_end)
+        occupied_slot = list(range(time_start_minutes, time_end_minutes+1))
+        for i in occupied_slot:
+            if i in calendar:
+                calendar.remove(i)
+        return calendar
 
-        pass
+
 
 
 if __name__ == '__main__':
@@ -30,7 +45,9 @@ if __name__ == '__main__':
     wl = Calendar.get_working_time([[8, 50], [17, 50]])
     print(*wl)
 
-    slotted = Calendar.book_timeslot(wl, [10, 30], [11, 30])
+    # slotted = Calendar.book_timeslot(wl, [10, 30], [11, 30])
+    #
+    # for i in slotted:
+    #     print(*i)
 
-    for i in slotted:
-        print(*i)
+    print(DAYTIME)
--- employee.py
+++ employee.py
@@ -3,16 +3,19 @@ from calendar import Calendar
 
 class Employee:
 
-    def __init__(self, name, role, location, working_time):
+    def __init__(self, name, role, location, working_time_list):
         self.name = name
         self.role = role
         self.location = location
-        self.working_time = Calendar.get_working_time(working_time)
-        self.calendar = Calendar.get_working_time(working_time)
+        self.working_time = Calendar.get_working_time(working_time_list)
+        self.calendar = Calendar.working_time_minutes(working_time_list)
 
 
 if __name__ == '__main__':
 
     mpopova = Employee('Maria Popova', 'accountant', 'Moscow', [[8, 50], [17, 30]])
     print(*mpopova.working_time)
+    print(mpopova.calendar)
+    mpopova.calendar = Calendar.book_timeslot(mpopova.calendar, [8, 50], [10, 0])
+    print(mpopova.calendar)
 
