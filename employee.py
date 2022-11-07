2f0d5a2cff0428c0bbdeffb2dd2f19897867124f
Svetlana Polyanskaya <s.s.polyanskaya@gmail.com>
2022-11-07 10:31:00 +0300

    gitignore added, __pycache__ and .idea are ignored now

--- employee.py
+++ employee.py
@@ -29,38 +29,43 @@ class Employee:
 
 if __name__ == '__main__':
 
-    mpopova = Employee('Maria Popova', 'accountant', 'Moscow', [[8, 50], [17, 30]])
-    # DataBaseEmulation.db_write('Maria Popova')
-    # Then mpopova object may be removed and the data about this employee should be fetched from the DB.
-    # It can be applied to all objects. But none of them is stired in the database.
-    # Assuming, that the object was removed:
-    # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
-    mpopova.calendar = Calendar.book_timeslot(mpopova.calendar, [8, 50], [9, 30])
-    # DataBaseEmulation.db_write('Maria Popova', ["calendar"])
-    # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
-    mpopova.calendar = Calendar.book_timeslot(mpopova.calendar, [11, 0], [11, 30])
-    # DataBaseEmulation.db_write('Maria Popova', ["calendar"])
-    # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
-    print("Maria's available timeslots: {}".format(Calendar.get_available_timeslots(mpopova.calendar)))
-    agreen = Employee('Alexander Green', 'programmer', 'Moscow', [[12, 0], [21, 0]])
-    # DataBaseEmulation.db_write('Alexander Green')
-    # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
-    agreen.calendar = Calendar.book_timeslot(agreen.calendar, [17, 0], [18, 8])
-    # DataBaseEmulation.db_write('Alexander Green', ["calendar"])
-    # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
-    print("Alexander's available timeslots: {}".format(Calendar.get_available_timeslots(agreen.calendar)))
-    sfishman = Employee('Sam Fishman', 'project manager', 'Moscow', [[10, 0], [19, 0]])
-    # DataBaseEmulation.db_write('Sam Fishman')
-    # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
-    sfishman.calendar = Calendar.book_timeslot(sfishman.calendar, [12, 0], [13, 0])
-    # DataBaseEmulation.db_write('Sam Fishman', ["calendar"])
-    # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
-    sfishman.calendar = Calendar.book_timeslot(sfishman.calendar, [13, 30], [14, 0])
-    # DataBaseEmulation.db_write('Sam Fishman', ["calendar"])
-    # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
-    print("Sam's available timeslots: {}".format(Calendar.get_available_timeslots(sfishman.calendar)))
-    # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
-    # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
-    # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
-    print('Common timeslots are: {}'.format(Calendar.get_common_timeslots(
-        [mpopova.calendar, agreen.calendar, sfishman.calendar])))
+    try:
+
+        mpopova = Employee('Maria Popova', 'accountant', 'Moscow', [[8, 50], [17, 30]])
+        # DataBaseEmulation.db_write('Maria Popova')
+        # Then mpopova object may be removed and the data about this employee should be fetched from the DB.
+        # It can be applied to all objects. But none of them is stired in the database.
+        # Assuming, that the object was removed:
+        # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
+        mpopova.calendar = Calendar.book_timeslot(mpopova.calendar, [8, 50], [9, 30])
+        # DataBaseEmulation.db_write('Maria Popova', ["calendar"])
+        # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
+        mpopova.calendar = Calendar.book_timeslot(mpopova.calendar, [11, 0], [11, 30])
+        # DataBaseEmulation.db_write('Maria Popova', ["calendar"])
+        # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
+        print("Maria's available timeslots: {}".format(Calendar.get_available_timeslots(mpopova.calendar)))
+        agreen = Employee('Alexander Green', 'programmer', 'Moscow', [[12, 0], [21, 0]])
+        # DataBaseEmulation.db_write('Alexander Green')
+        # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
+        agreen.calendar = Calendar.book_timeslot(agreen.calendar, [17, 0], [18, 8])
+        # DataBaseEmulation.db_write('Alexander Green', ["calendar"])
+        # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
+        print("Alexander's available timeslots: {}".format(Calendar.get_available_timeslots(agreen.calendar)))
+        sfishman = Employee('Sam Fishman', 'project manager', 'Moscow', [[10, 0], [19, 0]])
+        # DataBaseEmulation.db_write('Sam Fishman')
+        # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
+        sfishman.calendar = Calendar.book_timeslot(sfishman.calendar, [12, 0], [13, 0])
+        # DataBaseEmulation.db_write('Sam Fishman', ["calendar"])
+        # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
+        sfishman.calendar = Calendar.book_timeslot(sfishman.calendar, [13, 30], [14, 0])
+        # DataBaseEmulation.db_write('Sam Fishman', ["calendar"])
+        # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
+        print("Sam's available timeslots: {}".format(Calendar.get_available_timeslots(sfishman.calendar)))
+        # mpopova_calendar = DataBaseEmulation.db_fetch('Maria Popova', ["calendar"])
+        # agreen_calendar = DataBaseEmulation.db_fetch('Alexander Green', ["calendar"])
+        # sfishman_calendar = DataBaseEmulation.db_fetch('Sam Fishman', ["calendar"])
+        print('Common timeslots are: {}'.format(Calendar.get_common_timeslots(
+            [mpopova.calendar, agreen.calendar, sfishman.calendar])))
+
+    except TypeError:
+        print('Key Type error')
